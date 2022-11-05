0ee28ad50b8c751804fc994805cbbad86276f615
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-11-05 16:52:52 +0300

    Get Data From Local Storage

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
 /*
-    *** Add Data To The Local Storage ***
-*/
-
-const addDataToLocalStorage = (id) =>{
+ *** Add Data To The Local Storage ***
+ */
+const addDataToLocalStorage = (id) => {
     // step1: create a object which one we want to store into local storage
     const shoppingCart = {};
-    
+
     // step2: get stored shopping cart from the local storage
     const storedCart = localStorage.getItem("shopping-cart");
 
     // step3: check it stored or not,if stored, assign this object to shopping cart
-    if(storedCart){
+    if (storedCart) {
         shoppingCart = JSON.parse(storedCart);
     }
 
     // step4: check id, is already exist on object or not, if exists increment quantity
     // otherwise, added this id as key and added value 1
     const quantity = shoppingCart[id];
-    if(quantity){
+    if (quantity) {
         const newQuantity = quantity + 1;
         shoppingCart[id] = newQuantity;
-    }else{
+    } else {
         shoppingCart[id] = 1;
     }
 
     // step5: set shopping cart into local storage
     localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
-}
+};
+
+/*
+ *** Get Data From Local Storage ***
+ */
+const getDataFromLocalStorage = () => {
+    // step1: create object for storing data from local storage
+    const shoppingCart = {};
+
+    // step2: get desire data from the local storage
+    const storedCart = localStorage.getItem("shopping-cart");
+
+    // step3: check storeCart it has or not in the local storage, if it has
+    // assign store data into shopping cart object
+    if (storedCart) {
+        shoppingCart = JSON.parse(storedCart);
+    }
+
+    return shoppingCart;
+};
